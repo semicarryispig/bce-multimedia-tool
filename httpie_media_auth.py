16273e11d24ead0820c83f7768b2867a481d0179
liumin08 <liumin08@baidu.com>
2017-06-26 14:17:51 +0800

    bce multimedia client tool

---
+++ httpie_media_auth.py
@@ -0,0 +1,57 @@
+"""
+Media auth plugin for HTTPie.
+
+"""
+import os
+import sys
+
+from httpie import ExitStatus
+from httpie.plugins import AuthPlugin
+from httpie.compat import bytes
+from mediaauth import mediaAuth
+
+
+KEY = 'AWS_ACCESS_KEY_ID'
+SECRET = 'AWS_SECRET_ACCESS_KEY'
+
+
+class BytesHeadersFriendlyMediaAuth(MediaAuth):
+    """
+    extends MediaAuth
+    """
+    def __call__(self, r):
+        for k, v in r.headers.items():
+            if isinstance(v, bytes):
+                r.headers[k] = v.decode('utf8')
+        return super(BytesHeadersFriendlyMediaAuth, self).__call__(r)
+
+
+class MediaAuthPlugin(AuthPlugin):
+    """
+    media auth plugin
+    """
+    name = 'media auth'
+    auth_type = 'media'
+    description = ''
+    auth_require = False
+    prompt_password = True
+
+    def get_auth(self, username=None, password=None):
+        """
+        get auth
+        """
+        access_key = os.environ.get(KEY) if username is None else username
+        secret = os.environ.get(SECRET) if password is None else password
+        if not access_key or not secret:
+            missing = []
+            if not access_key:
+                missing.append(KEY)
+            if not secret:
+                missing.append(SECRET)
+            sys.stderr.write(
+                'httpie-aws-auth error: missing {1}\n'
+                    .format(self.name, ' and '.join(missing))
+            )
+            sys.exit(ExitStatus.PLUGIN_ERROR)
+
+        return BytesHeadersFriendlyMediaAuth(access_key, secret)
---
+++ mediaauth.py
@@ -0,0 +1,77 @@
+"""
+This module provides media auth
+"""
+import datetime
+import bcesigner
+import logging
+try:
+    from urlparse import urlparse
+except:
+    from urllib.parse import urlparse
+import uuid
+
+from requests.auth import AuthBase
+
+
+class MediaAuth(AuthBase):
+    """
+    media auth extends authBase
+    """
+    service_base_url = 'localhost'
+
+    def __init__(self, access_key, secret_key, service_url=None):
+        if service_url:
+            self.service_base_url = service_url
+        self.access_key = str(access_key)
+        self.secret_key = str(secret_key)
+
+    def __call__(self, r):
+        """
+        :param r:
+        :return:
+        """
+        parsedurl = urlparse(r.url)
+        uri = parsedurl.path
+        request = self.get_request(r.method, uri)
+        request = self.generate_signature(request)
+        r.headers['x-bce-date'] = request['headers']['x-bce-date']
+        r.headers['x-bce-request-id'] = request['headers']['x-bce-request-id']
+        r.headers['host'] = request['headers']['host']
+        r.headers['content-type'] = request['headers']['content-type']
+        r.headers['authorization'] = request['headers']['authorization']
+        r.params = request['params']
+        r.method = request['method']
+        r.uri = request['uri']
+        return r
+
+    def generate_signature(self, request):
+        """
+        :param request:
+        :return:
+        """
+        signer = bcesigner.BceSigner(self.access_key, self.secret_key)
+        signer.logger.setLevel(logging.WARNING)
+        auth = signer.gen_authorization(request, timestamp=request['headers']['x-bce-date'])
+
+        request['headers']['authorization'] = auth
+        request['headers']['x-bce-request-id'] = str(uuid.uuid4())
+        request['headers']['content-type'] = 'application/json'
+        return request
+
+    def get_request(self, method='', uri=''):
+        """
+        :param method:
+        :param uri:
+        :return:
+        """
+        return {
+            'method': method,
+            'uri': uri,
+            'params': {
+            },
+            'headers': {
+                'x-bce-date': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'host': self.service_base_url,
+                'content-type': 'application/json'
+            }
+        }
